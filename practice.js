bbdb38ae47d46fcb2e1354e32c2e17fb9e5ce8cb
Richard Pitts <richardepitts@gmail.com>
2019-08-03 22:47:34 +0000

    Automatically backed up by Learn

---
+++ practice.js
@@ -0,0 +1,48 @@
+function takeANumber(line, name) { 
+
+return `Welcome, {name}, you are number {line.length} in line`
+  
+}
+
+function takeANumber(katzDeliLine, name) {
+      katzDeliLine.push(name)
+      return "Welcome, " + name + ". You are number " + (katzDeliLine.indexOf(name) + 1) + " in line."
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine[0] == nil) {
+    return "There is nobody waiting to be served!"
+  } else {
+      var b = katzDeliLine[0]
+      return b
+      katzDeliLine.remove[0]
+}
+}
+
+function currentLine(lineOPeeps) {
+  if (lineOPeeps === undefined) {
+    return "The line is currently empty.";
+  } else { 
+    i = 0;
+    while (i < lineOPeeps.length) {
+    i++;
+    return "The line is currently:" + {i} + ":" + {lineOPeeps[0]
+    
+    
+    
+    
+  }
+  }
+  }
+
+  
+  
+
+  
+
+
+
+
+
+
+
